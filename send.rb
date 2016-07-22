c88797f88db6dcd833de37d35021fbd1915ab631
Evandro Junior <evandrojr@gmail.com>
2016-07-22 06:48:07 -0300

    Desafio de Amar 1

--- send.rb
+++ send.rb
@@ -10,8 +10,6 @@ password=ENV['gmailpass']
 `wget http://evandro.org/pilula/love_pill.sqlite3`
 
 
-
-
 pills=['Elogiar com sinceridade e carinho',
       'Jantar fora fazendo um clima romântico',
       'Café na cama caprichado',
@@ -33,8 +31,8 @@ pills=['Elogiar com sinceridade e carinho',
 prng = Random.new
 last_email = nil
 begin
-  db = SQLite3::Database.new "love_pill.test.sqlite3"
-#  db = SQLite3::Database.new "love_pill.sqlite3"
+  # db = SQLite3::Database.new "love_pill.test.sqlite3"
+   db = SQLite3::Database.new "love_pill.sqlite3"
   puts db.get_first_value 'SELECT SQLITE_VERSION()'
   stm = db.prepare "SELECT email from user"
   rs = stm.execute
@@ -44,12 +42,30 @@ begin
       email = gmail.generate_message do
         from "evandrojr@gmail.com"
         to row[0]
-        subject "Pílula do amor D&M - Tema da semana: Como manter a paixão no casamento"
-        body "Olá!
+        subject "Pílula do amor D&M - Série: O Desafio de Amar"
+        body "Queridos casais,
 
-Aqui vai a sugestão de Dengo (Evandro) e Modesta (Paulinha) para dar uma aquecida na sua relação. Experimente fazer isto durante esta semana.
+A partir dessa semana convidamos vocês a embarcarem em uma jornada inesquecível de descoberta sobre o verdadeiro Amor.  Baseado no livro “O Desafio de Amar” de Stephen e Alex Kendrick, citado no filme “Prova de Fogo”, enviaremos juntamente com as “Pílulas do Amor”  um “Devocional Semanal”   para maridos e esposas que desejam ardentemente, assim como nós, entender e viver o amor incondicional em seu casamento.
+Casais tripulantes do Amor, desafiem-se a aceitar o desafio de amar e veja o seu casamento ser transformado para sempre!!!!
 
-Sua pílula do amor é:
+1° SEMANA
+
+O Amor é paciente
+
+Sejam completamente humildes e dóceis, e sejam pacientes, suportando uns aos outros com amor. - Efésios 4:2
+
+Contradizendo o que muitos pensam, essa jornada nos remete a entender o amor não apenas como um sentimento, mas como uma AÇÃO.  Amar é um verbo de ação, desafiar-se a amar requer decisão. Basear nosso comportamento a partir da decisão de amar não é fácil, é realmente um desafio, mas a melhor  recompensa será  ter o verdadeiro AMOR fluindo em nossas vidas , em nosso casamento e  em toda nossa família.
+
+O amor é construído sobre dois pilares que melhor definem o que ele é. Esses pilares são a paciência e a bondade . Todas as outras características do amor são extensões desses dois atributos. E é aqui que começa o seu desafio, com a paciência.
+Quando você decide ser paciente, você responde de maneira positiva à uma situação negativa. Você é tardio em irar-se. Prefere ter um “pavio longo” a se irritar facilmente. Ao invés de ser impaciente e exigente, o amor lhe ajuda a se acalmar e a transmitir misericórdia aos que estão ao seu redor. A paciência traz a calma interior em meio a tempestade exterior.
+A paciência paralisa o andamento do problema. A paciência, mais do que morder a língua, mais do que bater a mão na boca, é respirar fundo. Ela purifica o ar. Ter paciência é escolher controlar suas emoções ao invés de permitir que elas lhe controlem. É demonstrar discrição ao invés de pagar mal com mal.
+A paciência nos torna sábios. Ela não se apressa em julgar, mas ouve o que a outra pessoa está dizendo. Ela espera na entrada, enquanto a raiva deseja invadir com violência. “O homem paciente dá prova de grande entendimento, mas o precipitado revela insensatez” (Provérbios 14:29).
+Assim como a falta de paciência fará de seu lar uma zona de guerra, a prática da paciência estimulará a paz e a tranquilidade. “O homem irritável provoca dissensão, mas quem é paciente acalma a discussão” (Provérbios 15:18).
+A paciência lhe ajuda a dar ao seu cônjuge o direito de ser humano. A paciência entende que todos falham. Poucos de nós praticam a paciência de forma adequada e nenhum de nós a pratica naturalmente. Mas o homem e a mulher sábios verão a paciência como um ingrediente essencial no casamento. Este é um bom ponto de partida para demonstrar amor verdadeiro.
+
+“Sejam todos prontos para ouvir, tardios para falar e tardios para irar-se”(Tiago 1:19).
+
+Também não esqueçam da pílula do amor da semana, seu cônjuge está recebendo outra diferente. Tente descobrir qual é a dele(a).
 
 --------------------------------------------------------------------------------
 
@@ -57,59 +73,7 @@ Sua pílula do amor é:
 
 --------------------------------------------------------------------------------
 
-10 segredos para manter a paixão no casamento
 
-Fonte: https://familia.com.br/7591/casamento/10-segredos-para-manter-a-paixao-no-casamento
-
-Sempre que ouvimos uma nova história de traição entre marido e mulher, alguém diz: \"Se traiu é porque não existia amor.\"
-
-Será?
-
-Se você plantar uma semente e largar, ela vai morrer. Da mesma forma, casamento e amor precisam de cuidados para crescer, florescer e criar raízes profundas. E isso inclui a paixão. Não aquela paixão do início, que não via defeitos nem conhecia o outro profundamente e tudo era lindo e perfeito. Mas a chama do casamento, aquela que lhe faz ter um friozinho na barriga quando o cônjuge chega perto, aquela que lhe faz olhar para o outro e admirá-lo, querer ficar junto, tocar, sentir, ouvir.
-
-Como manter isso após anos de convivência, problemas financeiros que virão, saúde dos familiares, filhos e desilusões? Aqui estão 10 segredos para saber e colocar em prática.
-
-1. Cumplicidade
-
-Antes de casar, ou mesmo depois, façam um acordo: Nada do que acontecer irá desunir você e seu amor. Mesmo que houver brigas, já decidam antes que ficarão juntos sempre. Quando um casal é cúmplice um do outro, isso pode ser notado até na maneira como se olham.
-
-2. Respeito
-
-Não somente respeito no sentido de fidelidade, mas respeito à opinião da pessoa, aos diferentes gostos, à história de vida que aquela pessoa tem, mesmo às experiências que trouxe para a vida a dois.
-
-3. Romantismo
-
-Quando um homem quer conquistar uma mulher, ele se perfuma, abre a porta do carro, segura sua mão ao andar. Quando uma mulher quer conquistar um homem, ela se cuida, fala baixo, olha com amor e sorri. Manter pequenos rituais de gentileza faz uma grande diferença ao longo dos anos.
-
-4. Diálogo
-
-Conforme a vida acontece, os afazeres tendem a distanciar corações. Normalmente nosso amor é nosso amigo, mas ninguém consegue adivinhar o que o outro está pensando. Mantenha o costume de conversar com seu cônjuge pelo menos alguns minutos por dia, independente do cansaço e agenda.
-
-5. Espaço conjunto
-
-Manter o espaço de vocês intocável faz toda a diferença. Pode haver épocas que as crianças e o cachorro venham dormir no meio de vocês, mas sempre que puder, mantenha o quarto como o refúgio do casal. Um local protegido por quatro paredes onde vocês podem ser vocês mesmos e resgatar o amor a qualquer hora. E usem-no para este propósito sempre!
-
-6. Buscar o progresso
-
-Isso significa cultivar uma atividade que você goste de fazer, seja algo que quer aprender, estudar, fazer, construir. Isso funciona como uma terapia na vida de cada um. Aprender a rir de si mesmo também ajuda na hora de exercer paciência e tolerância, sem achismos e julgamentos, no relacionamento com toda a família.
-
-7. Celebrações
-
-Comemorem as datas importantes. Eu e meu marido somos casados há quase 25 anos, e todo mês saímos para jantar na data de aniversário de casamento. Nem sempre foi possível sair, muitas vezes foi conversando sob as estrelas enrolados em cobertores enquanto as crianças assistiam um desenho na TV após o jantar. Outras vezes foi comendo pipoca e assistindo um filme juntos depois que as crianças dormiram. Muitas vezes, aquela data foi a única durante todo o mês que tivemos tempo e disposição de ficarmos a sós e lembrarmos dos laços que nos unem. Mas nunca deixamos de lembrar da data que finalmente conseguimos ficar juntos e decidimos ser o porto seguro um do outro. Não tenho palavras para descrever como isso ajudou nosso casamento até hoje.
-
-8. Jogar fora do dicionário a palavra \"divórcio\"
-
-No mundo afora se algo não vai bem no casamento, logo se fala em divórcio. Não jogue fora, conserte. Pense bem antes de cometer algo que irá machucar o outro. Tome boas decisões. A segurança que isso passará ao outro o ajudará a dedicar-se a cada dia para melhorar ainda mais o relacionamento.
-
-9. Rituais e tempo com a família
-
-Tradições de todos os tipos, diárias ou anuais, mantenha todas com os filhos e individualmente com cada um e com o outro. Não precisam fazer sempre as mesmas coisas, podem variar. O importante é fazê-las. Inclua o riso, o abraço, a oração, a lealdade, o carinho e humildade.
-
-10. Demonstre amor todos os dias
-
-Sim, todos os dias. Pode ser um beijo na hora de sair ou chegar, um bilhete, um SMS, uma rosa, um jantar, deitar no peito dele alguns minutos antes de dormir, uma sobremesa. Pode ser também perdoar, dar uma segunda chance, ter paciência, ouvir, olhar nos olhos. São pequenas doses de adubo na plantinha chamada \"amor\" que são extremamente necessárias para fazer as raízes se aprofundarem. Para melhor entendimento, leia o artigo 5 maneiras infalíveis de demonstrar amor.
-
-Vocês podem fazer tudo isso. Todavia, não há segredo em uma coisa: Os dois, marido e mulher, precisam se empenhar para que a semente se transforme numa árvore forte e firme, que dê bons frutos, sombra e proteção das interpéries da vida.
 Semana que vem tem mais!
 
 Seu parceiro ainda não está recebendo as pílulas por email? Sinto muito, você está deixando de receber vários benefícios para você e para sua relação.
